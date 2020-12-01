67a1b4426f82a4a898929f2c8e8e89d80f3767ae
Simon Painter <sjpainter19_uk@yahoo.co.uk>
2020-12-01 18:00:09 +0000

    add 2020 Day1 Part 2

--- FunctionalAdventOfCode/2020/AOC_2020_Day01.cs
+++ FunctionalAdventOfCode/2020/AOC_2020_Day01.cs
@@ -14,6 +14,17 @@ namespace FunctionalAdventOfCode
             input.ToHashSet()
                 .Map(x => x.First(y => x.Contains(2020 - y)))
                 .Map(x => x * (2020 - x));
+
+        public static int GetAuditChecksum3(IEnumerable<int> input)
+        {
+            var hashSet = input.ToHashSet();
+            var numbersBelow1000 = input.Where(x => x < 1000).ToArray();
+            var allPossibleCombos = numbersBelow1000.SelectMany(x => numbersBelow1000.Select(y => (x, y)));
+            var answer = allPossibleCombos.First(x => hashSet.Contains(2020 - x.x - x.y));
+            var returnValue = answer.x * answer.y * (2020 - answer.x - answer.y);
+            return returnValue;
+
+        }
     }
 
     public class AOC_2020_Day01
@@ -25,7 +36,17 @@ namespace FunctionalAdventOfCode
                 .Split(Environment.NewLine)
                 .Select(x => x.ToInt());
             var answer = AOC_2020_Day01_Answer.GetAuditChecksum(input);
-            answer.Should().Be(0);
+            answer.Should().Be(913824);
+        }
+
+        [Fact]
+        public void Day01b()
+        {
+            var input = File.ReadAllText(".//Content2//2020/Day01.txt")
+                .Split(Environment.NewLine)
+                .Select(x => x.ToInt());
+            var answer = AOC_2020_Day01_Answer.GetAuditChecksum3(input);
+            answer.Should().Be(240889536);
         }
     }
 }
